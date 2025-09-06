a909bd64899fe91d375e8dd730cb2f7ed7a3b700
louissalas10 <99519764+louissalas10@users.noreply.github.com>
2025-09-06 15:19:55 -0500

    Updates to Code

--- SalasTequilaMain/src/Inventory.java
+++ SalasTequilaMain/src/Inventory.java
@@ -4,6 +4,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Inventory{
  
@@ -14,31 +16,152 @@ public class Inventory{
 		private ResultSet rs = null;
 		
 		
-		public String viewInventory() {
+		public void viewInventory() throws SQLException  {
 			
 			System.out.println("Viewing...");
 			String I = "";
 			try {
 			
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/salas_tequila", "root", "root");
+			statement = connect.createStatement();
+			rs = statement.executeQuery("Select * From current_inventory");
+			
+			while (rs.next()) {
+			int productId = rs.getInt("product_id");
+			String name = rs.getString("product_name");
+			String product_Description = rs.getString("product_description");
+			Double prize = rs.getDouble("prize");
+			Double size_ML = rs.getDouble("size_ML");
+			int SKU = rs.getInt("SKU");
+			int quantity = rs.getInt("Quantity");
+			I = I + "Prodcut Id "+ productId + "Prodcut Name " + name + "Product Description" + product_Description + "Prize" + prize + "Size(mL)" + size_ML + "SKU" + SKU + "Quantity" + quantity + '\n';
+			System.out.println("Product ID: " + productId + ", Product Name: " + name + ", Prodcut Description: " + product_Description + ", Prize: " + prize + ", Size(mL): " + size_ML + ", SKU: " + SKU + "Quantity: " + quantity);} 
+			
+			
+			} catch (InputMismatchException e) {
+		        System.out.println("Error: Invalid input type. Please enter text.");}
+		
+		}
+		
+		public void addItem() throws SQLException{
+			String A = "";
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Product ID: ");
+			int ID = scanner.nextInt();
+			System.out.println("Product Name: ");
+			String pName = scanner.next();
+			System.out.println("Product Description: ");
+			String description = scanner.next();
+			System.out.println("Prize: ");
+			Double prize = scanner.nextDouble();
+			System.out.println("Size: ");
+			Double size = scanner.nextDouble();
+			System.out.println("SKU: ");
+			int SKU = scanner.nextInt();
+			System.out.println("Quantity: ");
+			int quantity = scanner.nextInt();
 			
 			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/salas_tequila", "root", "root");
 			statement = connect.createStatement();
-			rs = statement.executeQuery("Select * from current_inventory");
 			
-			while(rs.next()) {
-			int productId = rs.getInt("product_id");
-			String name = rs.getString("product_name");
-			I = I + "Prodcut Id "+ productId + "Prodcut Name " + name + '\n';
-			System.out.println("Product ID: " + productId + ", Product Name: " + name);
-			}
+			String query = ("Insert Into current_inventory(product_id, product_name, product_description, prize, size_ML, SKU, Quantity) Values(?, ?, ?, ?, ?, ?, ?);");
 			
-			}
-			catch(Exception ex) {System.out.println("Error " + ex.getMessage());}
+			PreparedStatement ps = connect.prepareStatement(query);
 			
-			return I;
+			ps.setInt(1,  ID);
+			ps.setString(2, pName);
+			ps.setString(3, description);
+			ps.setDouble(4, prize);
+			ps.setDouble(5, size);
+			ps.setInt(6, SKU);
+			ps.setInt(7, quantity);
+			ps.addBatch();
+			ps.executeBatch();
+			A = A +  "Prodcut Id "+ ID + "Prodcut Name " + pName + "Product Description" + description + "Prize" + prize + "Size(mL)" + size + "SKU" + SKU + "Quantity" + quantity + '\n';
+			System.out.println("Product ID: " + ID + ", Product Name: " + pName + ", Product Description: " + description + ", Prize: " + prize + ", Size(mL): " + size + ", SKU: " + SKU + ", Quantity: " + quantity);}
+			
+			
+			
+			public String update() throws SQLException {
+				String U = "";
+				
+				
+				
+				System.out.println("What Product ID do you want to update?");
+				Scanner scanner = new Scanner(System.in);
+				int newId = scanner.nextInt();
+				System.out.println("Updating Product ID # " + newId + "....");
+				System.out.println("Update to what quantity?");
+				int quantity = scanner.nextInt();
+				
+				connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/salas_tequila", "root", "root");
+				statement = connect.createStatement();
+
+			     Statement stmt = connect.createStatement();
+			        
+		            // Updating database
+		            String q1 = "UPDATE current_inventory set Quantity = '" + quantity + 
+		                    "' WHERE product_id = '" +newId+ "'";
+		            int x = stmt.executeUpdate(q1);
+		            
+		            if (x > 0)            
+		                System.out.println("Password Successfully Updated");            
+		            else            
+		                System.out.println("ERROR OCCURRED :(");
+		            rs = statement.executeQuery("Select * From current_inventory");
+		            
+			
+				while (rs.next()) {
+					int productId = rs.getInt("product_id");
+					String name = rs.getString("product_name");
+					String product_Description = rs.getString("product_description");
+					Double prize = rs.getDouble("prize");
+					Double size_ML = rs.getDouble("size_ML");
+					int SKU = rs.getInt("SKU");
+					int Quantity = rs.getInt("Quantity");
+					
+				U = U + "Prodcut Id "+ productId + "Prodcut Name " + name + "Product Description" + product_Description + "Prize" + prize + "Size(mL)" + size_ML + "SKU" + SKU + "Quantity" + quantity + '\n';
+				System.out.println("Product ID: " + productId + ", Product Name: " + name + ", Prodcut Description: " + product_Description + ", Prize: " + prize + ", Size(mL): " + size_ML + ", SKU: " + SKU + ", Quantity: " + quantity);} 
+				//rs = statement.executeQuery ("UPDATE current_inventory SET Quantity = '" + quantity + "' WHERE product_id = '" + newId + "'");
+				
+				return U;
 			}
 		
-			}
+			
+		public String search() throws SQLException {
+			
+			String S = "";
+			System.out.println("Search: ");
+			Scanner scanner = new Scanner(System.in);
+			String name1 = scanner.next();
+			
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/salas_tequila", "root", "root");
+			statement = connect.createStatement();
+			rs = statement.executeQuery("Select * From current_inventory Where product_description Like '%" + name1 + "%'");
+			
+
+			while(rs.next()) {
+			int productId = rs.getInt("product_id");
+			String productName = rs.getString("product_name");
+			String product_Description = rs.getString("product_description");
+			Double prize = rs.getDouble("prize");
+			Double size_ML = rs.getDouble("size_ML");
+			int SKU = rs.getInt("SKU");
+			int quantity = rs.getInt("Quantity");
+			S = S +  "Prodcut Id "+ productId + "Prodcut Name " + productName + "Product Description" + product_Description + "Prize" + prize + "Size(mL)" + size_ML + "SKU" + SKU +  "Quantity" + quantity +  '\n';
+			System.out.println("Product ID: " + productId + ", Product Name: " + productName + ", Prodcut Description: " + product_Description + ", Prize: " + prize + ", Size(mL): " + size_ML + ", SKU: " + SKU + ", Quantity: " + quantity);
+			} 
+			
+			
+			return S;
+		}
+		
+			
+			
+
+	 }
+
+
 		
 		
 		
--- SalasTequilaMain/src/Main.java
+++ SalasTequilaMain/src/Main.java
@@ -22,13 +22,13 @@ int choice = '0';
 
 do{
 	//Menu
-	System.out.println("1. Enter 1 to view all of current inventory.");
-	System.out.println("2. Enter 2 to add new item(s) to inventory");
-	System.out.println("3. Enter 3 to update what is in stock.");
-	System.out.println("4. Enter 4 to generate a report.");
-	System.out.println("5. Enter 5 to search for a specific item.");
-	System.out.println("6. Enter 6 to contact customer support.");
-	System.out.println("7. quit.");
+	System.out.println("1. Enter 1 to View All Of Current Inventory.");
+	System.out.println("2. Enter 2 to Add New Item(s) To Inventory");
+	System.out.println("3. Enter 3 to Update What Is In Stock.");
+	System.out.println("4. Enter 4 to Generate A Report.");
+	System.out.println("5. Enter 5 to Search For A Specific Item.");
+	System.out.println("6. Enter 6 to Contact Customer Support.");
+	System.out.println("7. Quit.");
 	
 	//declares variable for user's choice
 	choice = user.nextInt();
@@ -39,27 +39,34 @@ do{
 		
 		Inventory I = new Inventory();
 		I.viewInventory();
-		
-	
 		break;
+		
 	case 2:
 		System.out.println("Adding….");
+		Inventory a = new Inventory();
+		a.addItem();
 		break;
+		
 	case 3:
 		System.out.println("Updating….");
+		Inventory u = new Inventory();
+		u.update();
 		break;
+		
+		
 	case 4:
 		System.out.println("Generating….");
 		break;
+		
 	case 5:
 		System.out.println("Searching….");
 			try {
-			Log myLog= new Log("mylog.txt");
-			
-			myLog.logger.setLevel(Level.INFO);
-			myLog.logger.warning("No information found");
+				Inventory S = new Inventory();
+				S.search();
+		
 		} catch (Exception e) {} 
 		break;
+		
 	case 6:
 		System.out.println("Customer Service");
 		break;
